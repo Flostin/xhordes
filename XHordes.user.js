17a2cc549228f47a9dd86deeaf73e49803b4b618
Campinator <joefakocamp@gmail.com>
2018-05-18 20:11:11 -0500

    :thinking:

--- XHordes.user.js
+++ XHordes.user.js
@@ -26,6 +26,7 @@
     });
 })();
 
+
 //Make the DIV element draggagle: (Ignore this, it isn't finished)
 // var script = document.createElement("script");
 // script.textContent = 'function dragElement(e){var t=0,n=0,o=0,d=0;function m(e){e=e||window.event,o=e.clientX,d=e.clientY,document.onmouseup=u,document.onmousemove=i}function i(m){m=m||window.event,t=o-m.clientX,n=d-m.clientY,o=m.clientX,d=m.clientY,e.offsetTop-n<0||e.offsetLeft-t<0||e.offsetLeft-t+e.getBoundingClientRect().width-15>document.body.clientWidth||e.getBoundingClientRect().bottom-n>window.innerHeight||(document.getElementById("x").textContent=e.getBoundingClientRect().bottom,document.getElementById("y").textContent=e.getBoundingClientRect().bottom-n,e.style.top=e.offsetTop-n+"px",e.style.left=e.offsetLeft-t+"px")}function u(){document.onmouseup=null,document.onmousemove=null,document.getElementById("x").textContent=e.getBoundingClientRect().bottom,document.getElementById("y").textContent=window.innerHeight}document.getElementById(e.id+"header")?document.getElementById(e.id+"header").onmousedown=m:e.onmousedown=m}dragElement(document.getElementById("ui_chat")),dragElement(document.getElementById("ui_inventory")),dragElement(document.getElementById("ui_char"));';
