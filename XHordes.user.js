f9d334f9dab57416a47d0451f21d4f4ab5e60bd9
Campinator <joefakocamp@gmail.com>
2019-03-20 19:43:04 -0500

    Add an "*" to make stuff work properly

--- XHordes.user.js
+++ XHordes.user.js
@@ -4,7 +4,7 @@
 // @version      0.6.1
 // @description  A modded version of the Hordes.io client
 // @author       LegusX and CampFire
-// @match        https://hordes.io/
+// @match        https://hordes.io/*
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM_xmlhttpRequest
